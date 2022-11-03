9dce467324c581b806f429c518fbe90d9fc080f3
Elnaraa9 <100670042+Elnaraa9@users.noreply.github.com>
2022-11-03 13:46:17 +0400

    Add files via upload

---
+++ jstask2/assets/js/main.js
@@ -0,0 +1,74 @@
+// 1st Task
+
+// let A=+prompt('Please enter the value:');
+// while (isNaN(A) || A == "") {
+//   A = +prompt("PLease enter the number");
+// }
+// for (i=1; i<1000; i++) {
+//    if (i%A==0) {
+//     console.log(`Answer: ${i}`);
+//    }
+// }
+
+//2nd Task
+
+// let A=+prompt('PLease enter the value');
+// while (isNaN(A) || A == "") {
+//   A = +prompt("PLease enter the number");
+// }
+// for(n=1; n<A; n++) {
+//     if (n*n<A) {
+//        console.log(`Answer: ${n}`);
+//     }
+// }
+
+//3rd Task
+
+// let A = +prompt("PLease enter the value");
+// while (isNaN(A) || A == "") {
+//   A = +prompt("PLease enter the number");
+// }
+// let largerestDivisor = 0;
+// for (i = 1; i < A; i++) {
+//   if (A % i==0) {
+//     largerestDivisor = i;
+//   }
+// }
+// console.log(`Largest divisor is: ${largerestDivisor}`);
+
+// 4th Task
+
+// let A = +prompt("PLease enter the value");
+// while (isNaN(A) || A == "") {
+//   A = +prompt("PLease enter the number");
+// }
+// let B = +prompt("PLease enter the value");
+// while (isNaN(B) || B == "") {
+//   B = +prompt("PLease enter the number");
+// }
+
+// let sum = 0;
+// if (A > B) {
+//   for (i = B; i < A; i++) {
+//     if (i % 7 == 0) {
+//       sum += i;
+//     }
+//   }
+//   console.log(`Answer: ${sum}`);
+// } else if (A < B) {
+//   console.log(`B can not be larger than A`);
+// } else {
+//   console.log(`B can not be equal A`);
+// }
+
+//5th Task
+
+//6th Task
+
+//7th Task
+
+// let A = +prompt("PLease enter the value");
+// while (isNaN(A) || A == "") {
+//   A = +prompt("PLease enter the number");
+// }
+// let revers=0;
